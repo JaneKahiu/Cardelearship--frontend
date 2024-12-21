d57b20997dfcd2eb9ee464643303b674fe5d1630
JaneKahiu <jkahiu2020@gmail.com>
2024-12-21 10:18:21 +0300

    Removed unused imports for About and OurServices

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import SignupForm from './components/SignupForm';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Login from './components/login';
 import HomePage from './components/home';
-import About from './components/about';
-import OurServices from './components/ourservices';
 import Cars from './components/Cars';
 import ContactUs from './components/contactus';
 import CustomerProfile from './components/CustomerProfile';
@@ -20,8 +18,6 @@ const App = () => {
                 <Route path="/signup" element={<SignupForm />} />
                 <Route path="/login"element={<Login />} />
                 <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/ourservices" element={<OurServices />} />
                 <Route path="/cars" element={<Cars />} />
                 <Route path="/contactus" element={<ContactUs />} />
                 <Route path="/customer-profile" element={<CustomerProfile />} />
